5ea3bd89a62420180684b7e8de95de5189df99eb
Salmatul Farida <101133297+SalmatulFarida@users.noreply.github.com>
2023-12-12 05:31:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,19 @@ import streamlit as st
 import time
 from generate_label import get_label
 
-
 def main():
-
     st.set_page_config(
         page_title="Aplikasi Kategori Berita | Klasifikasi Berita AntaraNews", page_icon="📺")
 
     col1, col2 = st.columns(2)
 
     with col1:
-
         st.image("assets/banner.png", use_column_width=True)
 
     with col2:
         st.subheader("News Classification: Aplikasi Kategori Berita Online (AntaraNews)")
-        st.caption("Berita online akan dikategorikan menjadi beberapa kategori seperti, hukum, hiburan, lifestyle. Dengan melakukan klasifikasi pada berita, kita dapat mengetahui kategori berita online yang sesuai.")
+        st.caption("Berita online akan dikategorikan menjadi beberapa kategori seperti hukum, hiburan, lifestyle. Dengan melakukan klasifikasi pada berita, kita dapat mengetahui kategori berita online yang sesuai.")
+
     news_text = st.text_area(
         "Masukkan Isi Berita", key="input_text", height=250)
 
@@ -25,26 +23,23 @@ def main():
         if news_text:
             text = get_label(news_text)
             with st.expander('Tampilkan Hasil'):
-                st.write('Berita yang anda masukkan termasuk dalam kategori: ')
+                st.write(f'Berita yang Anda masukkan termasuk dalam kategori: {text}')
+                url = ""
+
                 if text == "hukum":
                     st.info(text, icon="🧑‍🏫")
                     url = "https://www.antaranews.com/search?q=hukum+hari+ini"
-                    st.write(
-                        'Baca juga berita terbaru terkait hukum 🔎 [Berita hukum hari ini](%s)'  %url)
                 elif text == "hiburan":
                     st.info(text, icon="🚣")
                     url = "https://www.antaranews.com/search?q=hiburan+hari+ini"
-                    st.write(
-                        'Baca juga berita terbaru terkait hiburan 🔎 [Berita hiburan hari ini](%s)'  %url)
                 elif text == "lifestyle":
                     st.info(text, icon="💸")
                     url = "https://www.antaranews.com/search?q=lifestyle+hari+ini"
-                    st.write(
-                        'Baca juga berita terbaru terkait lifestyle 🔎 [Berita lifestyle hari ini](%s)'  %url)
-        else:
-            time.sleep(.5)
-            st.toast('Masukkan teks terlebih dahulu', icon='🤧')
 
+                st.write(f'Baca juga berita terbaru terkait {text} 🔎 [Berita {text} hari ini]({url})')
+        else:
+            time.sleep(0.5)
+            st.warning('Masukkan teks terlebih dahulu')
 
 if __name__ == "__main__":
     main()
