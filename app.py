9c82df99ce5ffa2f93ba9b690a559046f1df1bfd
Salmatul Farida <101133297+SalmatulFarida@users.noreply.github.com>
2023-12-12 10:40:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,56 @@
-# main.py
 import streamlit as st
-import time
-from generate_label import get_label
+import numpy as np
+import pandas as pd
+import joblib
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
+import altair as alt
+#from streamlit_option_menu import option_menu
+from joblib import load
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+showWarningOnDirectExecution = False
+
+st.title('Portal Tugas UAS Berita PPW')
+
+st.write("Oleh | Salmatul Farida | 200411100016")
+tab1, tab2, tab3, tab4 = st.tabs(["Data Mentah", "Preprocessing","Summary", "Modelling"])
 
 
-def main():
-
-    st.set_page_config(
-        page_title="Aplikasi Kategori Berita | Klasifikasi Berita AntaraNews", page_icon="📺")
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-
-        st.image("assets/antara.png", use_column_width=True)
-
-    with col2:
-        st.subheader("News Classification: Aplikasi Kategori Berita Online (AntaraNews)")
-        st.caption("Berita online akan dikategorikan menjadi beberapa kategori seperti, hukum, hiburan, lifestyle. Dengan melakukan klasifikasi pada berita, kita dapat mengetahui kategori berita online yang sesuai.")
-    news_text = st.text_area(
-        "Masukkan Isi Berita", key="input_text", height=250)
-
-    if st.button("Cari Kategori"):
-        if news_text:
-            text = get_label(news_text)
-            with st.expander('Tampilkan Hasil'):
-                st.write('Berita yang anda masukkan termasuk dalam kategori: ')
-                if text == "hukum":
-                    st.info(text, icon="🧑‍🏫")
-                    url = "https://www.antaranews.com/search?q=hukum+hari+ini"
-                    st.write(
-                        'Baca juga berita terbaru terkait hukum 🔎 [Berita hukum hari ini](%s)'  %url)
-                elif text == "hiburan":
-                    st.info(text, icon="🚣")
-                    url = "https://www.antaranews.com/search?q=hiburan+hari+ini"
-                    st.write(
-                        'Baca juga berita terbaru terkait hiburan 🔎 [Berita hiburan hari ini](%s)'  %url)
-                elif text == "lifestyle":
-                    st.info(text, icon="💸")
-                    url = "https://www.antaranews.com/search?q=lifestyle+hari+ini"
-                    st.write(
-                        'Baca juga berita terbaru terkait lifestyle 🔎 [Berita lifestyle hari ini](%s)'  %url)
-        else:
-            time.sleep(.5)
-            st.toast('Masukkan teks terlebih dahulu', icon='🤧')
+# ====================== Crawling ====================
+with tab1:
+        st.caption('Dataset diambil dari crawling menggunakan library Beautifulsoap dari situs radarjatim.com, dengan 3 kategori yaitu: Pendidikan, Relisi dan Sosial')
+        dataset = pd.read_csv("https://raw.githubusercontent.com/SalmatulFarida/PPW/gh-pages/crawling_berita_antaranews.csv")
+        st.dataframe(dataset)
+        st.info(f"Banyak Dataset : {len(dataset)}")
+        st.warning(f'Informasi Dataset')
+        st.write(dataset.describe())
 
 
-if __name__ == "__main__":
-    main()
+with tab2:
+        dataset = pd.read_csv("https://raw.githubusercontent.com/SalmatulFarida/PPW/gh-pages/Datasummary-antaranews2.csv")
+        st.dataframe(dataset)
+        st.info(f"Banyak Dataset : {len(dataset)}")
+        st.warning(f'Informasi Dataset')
+        st.write(dataset.describe())
+
+
+with tab3:
+        dataset = pd.read_csv("https://raw.githubusercontent.com/SalmatulFarida/PPW/gh-pages/DataSummaryBerita.csv")
+        
+        st.dataframe(dataset)
+        st.info(f"Banyak Dataset : {len(dataset)}")
+        st.warning(f'Informasi Dataset')
+        st.write(dataset.describe())
+
+with tab4:
+        dataset = pd.read_csv("hhttps://raw.githubusercontent.com/SalmatulFarida/PPW/gh-pages/hasilPrediksi.csv")
+        
+        st.dataframe(dataset)
+        st.info(f"Banyak Dataset : {len(dataset)}")
+        st.warning(f'Informasi Dataset')
+        st.write(dataset.describe())
